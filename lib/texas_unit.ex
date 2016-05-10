31499b6db3da8701658e6509ceda786a602e2ed7
Michael Pope <mpope.cr@gmail.com>
2016-05-10 09:53:56 -0700

    Add Rspec style 'describe' test

--- lib/texas_unit.ex
+++ lib/texas_unit.ex
@@ -3,12 +3,27 @@ defmodule TexasUnit do
     quote do
       use ExUnit.Case
       import TexasUnit
+      Module.put_attribute(__MODULE__, :stack, [])
+    end
+  end
+
+  defmacro describe(description, block) do
+    quote do
+      old_stack = Module.get_attribute(__MODULE__, :stack)
+      new_stack = [unquote(description)|old_stack]
+      Module.put_attribute(__MODULE__, :stack, new_stack)
+      unquote(block)
+      Module.put_attribute(__MODULE__, :stack, old_stack)
     end
   end
 
   defmacro it(description, block) do
     quote do
-      ExUnit.Case.test(unquote(description), unquote(block))
+      full_description =
+        [unquote(description)|Module.get_attribute(__MODULE__, :stack)]
+        |> Enum.reverse
+        |> Enum.join(" ")
+      ExUnit.Case.test(full_description, unquote(block))
     end
   end
 end
--- test/texas_unit_test.exs
+++ test/texas_unit_test.exs
@@ -8,4 +8,18 @@ defmodule TexasUnitTest do
   it "RSpec style \"it\" works" do
     assert :this_test_compiled == :this_test_compiled
   end
+
+  describe "RSpec style \"describe\" wrapped around" do
+    it "RSpec style \"it\" works" do
+      assert :this_test_compiled == :this_test_compiled
+    end
+  end
+
+  describe "RSpec style \"describe\" wrapped around" do
+    describe "RSpec style \"describe\" wrapped around" do
+      it "RSpec style \"it\" works" do
+        assert :this_test_compiled == :this_test_compiled
+      end
+    end
+  end
 end
